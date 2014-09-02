be1ba3ec5a353eaa80175a568f6cfc20103df2f2
Ted Benson <eob@csail.mit.edu>
2014-09-02 10:42:37 -0400

    Initial move

--- src/engine.js
+++ src/engine.js
@@ -1,5 +1,5 @@
-// Engine
-// ==========================================================================
+var Util = require('cts/util');
+var Factory = require('./factory');
 
 /*
  * Available options:
@@ -24,16 +24,16 @@ var Engine = CTS.Engine = function(opts, args) {
   this.initialize.apply(this, args);
 
   this.status = {
-    defaultTreeReady: CTS.Promise.defer(),
-    allRealized: CTS.Promise.defer(),
-    booted: CTS.Promise.defer(),
-    rendered: CTS.Promise.defer()
+    defaultTreeReady: Util.Promise.defer(),
+    allRealized: Util.Promise.defer(),
+    booted: Util.Promise.defer(),
+    rendered: Util.Promise.defer()
   };
 };
 
 // Instance Methods
 // ----------------
-CTS.Fn.extend(Engine.prototype, Events, {
+Util._.extend(Engine.prototype, Events, {
 
   initialize: function() {
   },
@@ -48,138 +48,138 @@ CTS.Fn.extend(Engine.prototype, Events, {
     var self = this;
     var pt = this.forrest.getPrimaryTree();
 
-    CTS.Log.Debug("CTS::Engine::render called on Primary Tree");
-    CTS.Log.Tick("CTS:Engine Render");
-    var options = CTS.Fn.extend({}, opts);
+    Util.Log.Debug("CTS::Engine::render called on Primary Tree");
+    Util.Log.Tick("CTS:Engine Render");
+    var options = Util._.extend({}, opts);
     pt.root._processIncoming().then(
       function() {
-        CTS.Log.Debug("CTS::Engine::render finished on Primary Tree");
-        CTS.Log.Tock("CTS:Engine Render");
+        Util.Log.Debug("CTS::Engine::render finished on Primary Tree");
+        Util.Log.Tock("CTS:Engine Render");
         self.status.rendered.resolve();
       },
       function(reason) {
-        CTS.Log.Error(reason);
+        Util.Log.Error(reason);
         self.status.rendered.reject(reason);
       }
     ).done();
   },
 
   boot: function() {
-    CTS.Log.Info("Engine: Starting Boot");
-    CTS.Log.Tick("CTS:Boot");
+    Util.Log.Info("Engine: Starting Boot");
+    Util.Log.Tick("CTS:Boot");
     this.bootStage = "Booting";
     var self = this;
     if (typeof self.booting != 'undefined') {
-      CTS.Error("Already booted / booting");
+      Util.Log.Error("Already booted / booting");
     } else {
       self.booting = true;
     }
     self.bootStage = "Loading Forrest";
-    CTS.Log.Tick("CTS:LoadForrest");
+    Util.Log.Tick("CTS:LoadForrest");
     self.loadForrest().then(function() {
-      CTS.Log.Debug("Engine: Loaded Forrest");
+      Util.Log.Debug("Engine: Loaded Forrest");
       self.bootStage = "Loading CTS";
-      CTS.Log.Tock("CTS:LoadForrest");
-      CTS.Log.Tick("CTS:LoadCTS");
+      Util.Log.Tock("CTS:LoadForrest");
+      Util.Log.Tick("CTS:LoadCTS");
       return self.loadCts();
     }).then(function() {
-      CTS.Log.Debug("Engine: Loaded CTS");
-      CTS.Log.Tock("CTS:LoadCTS");
-      CTS.Log.Tick("CTS:RealizeDependencies");
+      Util.Log.Debug("Engine: Loaded CTS");
+      Util.Log.Tock("CTS:LoadCTS");
+      Util.Log.Tick("CTS:RealizeDependencies");
       self.bootStage = "Realizing Dependencies";
       return self.forrest.realizeDependencies();
     }).then(function() {
-      CTS.Log.Debug("Engine: Realized Dependencies");
-      CTS.Log.Tock("CTS:RealizeDependencies");
-      CTS.Log.Tick("CTS:RealizeTrees");
+      Util.Log.Debug("Engine: Realized Dependencies");
+      Util.Log.Tock("CTS:RealizeDependencies");
+      Util.Log.Tick("CTS:RealizeTrees");
       self.bootStage = "Realize Trees";
       return self.forrest.realizeTrees();
     }).then(function() {
-      CTS.Log.Debug("Engine: Realized Trees");
+      Util.Log.Debug("Engine: Realized Trees");
       self.bootStage = "Realize Relations";
-      CTS.Log.Tock("CTS:RealizeTrees");
-      CTS.Log.Tick("CTS:RealizeRelations");
-      return CTS.Promise.fcall(function() {
+      Util.Log.Tock("CTS:RealizeTrees");
+      Util.Log.Tick("CTS:RealizeRelations");
+      return Util.Promise.fcall(function() {
         self.forrest.realizeRelations()
       });
     }).then(function() {
       self.status.allRealized.resolve();
-      CTS.Log.Tock("CTS:RealizeRelations");
-      CTS.Log.Info("Engine: CTS Realized Relations. Starting Render.");
+      Util.Log.Tock("CTS:RealizeRelations");
+      Util.Log.Info("Engine: CTS Realized Relations. Starting Render.");
       self.bootStage = "Render";
       self.render.call(self);
       self.bootStage = "Finalizing Boot";
-      CTS.Log.Tock("CTS:Boot");
+      Util.Log.Tock("CTS:Boot");
       self.status.booted.resolve();
-      return CTS.Promise.fcall(function() { return true; });
+      return Util.Promise.fcall(function() { return true; });
     }).fail(function(error) {
-      CTS.Log.Error("Boot stage failed.", error);
+      Util.Log.Error("Boot stage failed.", error);
       self.status.booted.reject(error);
     }).done();
     return self.status.booted;
   },
 
   renderlessBoot: function() {
-    CTS.Log.Info("Engine: Starting Boot");
-    CTS.Log.Tick("CTS:Boot");
+    Util.Log.Info("Engine: Starting Boot");
+    Util.Log.Tick("CTS:Boot");
     this.bootStage = "Booting";
     var self = this;
     if (typeof self.booting != 'undefined') {
-      CTS.Error("Already booted / booting");
+      Util.Log.Error("Already booted / booting");
     } else {
       self.booting = true;
     }
     self.bootStage = "Loading Forrest";
-    CTS.Log.Tick("CTS:LoadForrest");
+    Util.Log.Tick("CTS:LoadForrest");
     self.loadForrest().then(function() {
       self.bootStage = "Loading CTS";
-      CTS.Log.Tock("CTS:LoadForrest");
-      CTS.Log.Tick("CTS:LoadCTS");
+      Util.Log.Tock("CTS:LoadForrest");
+      Util.Log.Tick("CTS:LoadCTS");
       return self.loadCts();
     }).then(function() {
       self.status.defaultTreeReady.resolve();
-      CTS.Log.Debug("Engine: Loaded CTS");
-      CTS.Log.Tock("CTS:LoadCTS");
-      CTS.Log.Tick("CTS:RealizeDependencies");
+      Util.Log.Debug("Engine: Loaded CTS");
+      Util.Log.Tock("CTS:LoadCTS");
+      Util.Log.Tick("CTS:RealizeDependencies");
       self.bootStage = "Realizing Dependencies";
       return self.forrest.realizeDependencies();
     }).then(function() {
-      CTS.Log.Debug("Engine: Realized Dependencies");
-      CTS.Log.Tock("CTS:RealizeDependencies");
-      CTS.Log.Tick("CTS:RealizeTrees");
+      Util.Log.Debug("Engine: Realized Dependencies");
+      Util.Log.Tock("CTS:RealizeDependencies");
+      Util.Log.Tick("CTS:RealizeTrees");
       self.bootStage = "Realize Trees";
       return self.forrest.realizeTrees();
     }).then(function() {
-      CTS.Log.Debug("Engine: Realized Trees");
+      Util.Log.Debug("Engine: Realized Trees");
       self.bootStage = "Realize Relations";
-      CTS.Log.Tock("CTS:RealizeTrees");
-      CTS.Log.Tick("CTS:RealizeRelations");
-      return CTS.Promise.fcall(function() {
+      Util.Log.Tock("CTS:RealizeTrees");
+      Util.Log.Tick("CTS:RealizeRelations");
+      return Util.Promise.fcall(function() {
         self.forrest.realizeRelations()
       });
     }).then(function() {
-      CTS.Log.Tock("CTS:RealizeRelations");
-      CTS.Log.Tock("CTS:Boot");
+      Util.Log.Tock("CTS:RealizeRelations");
+      Util.Log.Tock("CTS:Boot");
       self.status.booted.resolve();
-      return CTS.Promise.fcall(function() { return true; });
+      return Util.Promise.fcall(function() { return true; });
     }).fail(function(error) {
-      CTS.Log.Error("Boot stage failed.", error);
+      Util.Log.Error("Boot stage failed.", error);
       self.status.booted.reject(error);
     }).done();
     return self.status.booted;
   },
 
   loadForrest: function() {
-    var promise = CTS.Promise.defer();
+    var promise = Util.Promise.defer();
     var self = this;
     if (typeof this.opts.forrest == 'undefined') {
       this.opts.forrest = {};
     }
     this.opts.forrest.engine = this;
-    CTS.Factory.Forrest(this.opts.forrest).then(
+    Factory.Forrest(this.opts.forrest, Factory).then(
       function(forrest) {
         self.forrest = forrest;
-        CTS.Log.Info("Engine: Resolved forrest.");
+        Util.Log.Info("Engine: Resolved forrest.");
         promise.resolve();
       }
     );
@@ -195,17 +195,17 @@ CTS.Fn.extend(Engine.prototype, Events, {
     }
     if (typeof this.forrest.trees[tree] == 'undefined') {
       if (typeof this.forrest.treeSpecs[tree] == 'undefined') {
-        CTS.Log.Error("Unknown tree: ", tree);
+        Util.Log.Error("Unknown tree: ", tree);
         return null;
       } else {
-        CTS.Log.Error("Tree was not realized from selection spec: ", tree);
+        Util.Log.Error("Tree was not realized from selection spec: ", tree);
         return null;
       }
     }
     console.log("Trying to get", selector, "from", tree);
     var nodes = this.forrest.trees[tree].root.find(selector);
     console.log("Got", nodes.length, "nodes");
-    var values = CTS.Fn.map(nodes, function(n) { return n.getValue(); });
+    var values = Util._.map(nodes, function(n) { return n.getValue(); });
     return values;
   },
 
@@ -219,13 +219,13 @@ CTS.Fn.extend(Engine.prototype, Events, {
     }
 
     if ((typeof self.opts.autoLoadSpecs != 'undefined') && (self.opts.autoLoadSpecs === true)) {
-      var links = CTS.Util.getTreesheetLinks();
+      var links = Util.Helper.getTreesheetLinks();
       var ps = self.forrest.parseAndAddSpecsFromLinks(links);
       for (var i = 0; i < ps.length; i++) {
         promises.push(ps[i]);
       }
     }
-    return CTS.Promise.all(promises);
+    return Util.Promise.all(promises);
   },
 
   // Stops all event listeners
---
+++ src/index.js
@@ -0,0 +1,2 @@
+exports.Factory = require('./factory');
+exports.Engine = require('./engine');
